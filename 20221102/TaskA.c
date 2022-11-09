86fd86adbfa5ae4f3d1e3dd86955d2137b8d0754
humihumiinoue <shyallandboill1212@gmail.com>
2022-11-09 15:04:56 +0900

    Task.c追加

---
+++ 20221102/TaskA.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//1から10まで表示
+int main()
+{
+    printf("Hello World\n");
+    
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ 20221102/TaskC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int i,random[10];
+int ransuu;
+
+    for(int i = 0; i < 10; i++)
+    {
+        random[i] = ransuu;
+        printf("%d\n",rand());
+    }
+    return 0;
+}
