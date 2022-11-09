db185459c13a15676932c6d37d77c06133be98a5
humihumiinoue <shyallandboill1212@gmail.com>
2022-11-09 15:18:30 +0900

    TaskD.ｃFizzBuzz作成

--- 20221102/TaskD.c
+++ 20221102/TaskD.c
@@ -2,5 +2,19 @@
 
 int main()
 {
-    
+    for(int i = 0; i < 100; i++)
+    {
+        if(i % 15 == 0)
+        {
+            printf("%d fizz buzz\n",i);
+        }
+        else if(i % 3 == 0)
+        {
+            printf("%d fizz\n",i);
+        }
+        else if(i % 5 == 0)
+        {
+           printf("%d buzz\n",i);
+        }
+    }
 }
